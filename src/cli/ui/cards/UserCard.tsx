5b03ee7543e7d197eb13f882331bac032a2b7ae6
CXG <cx501560@gmail.com>
2026-07-24 23:09:49 +0800

    fix(ui): frame user history messages

--- src/cli/ui/cards/UserCard.tsx
+++ src/cli/ui/cards/UserCard.tsx
@@ -4,12 +4,19 @@ import React from "react";
 import { t } from "../../../i18n/index.js";
 import { CardHeader } from "../primitives/CardHeader.js";
 import type { UserCard as UserCardData } from "../state/cards.js";
-import { CARD, FG, MESSAGE_BG } from "../theme/tokens.js";
+import { CARD, FG, SURFACE, TONE } from "../theme/tokens.js";
 import { formatRelativeTime } from "./time.js";
 
 export function UserCard({ card }: { card: UserCardData }): React.ReactElement {
   return (
-    <Box flexDirection="column" width="100%" paddingX={1} backgroundColor={MESSAGE_BG.user}>
+    <Box
+      flexDirection="column"
+      width="100%"
+      paddingX={1}
+      borderStyle="round"
+      borderColor={TONE.brand}
+      backgroundColor={SURFACE.bgInput}
+    >
       <CardHeader
         glyph={CARD.user.glyph}
         tone={CARD.user.color}
